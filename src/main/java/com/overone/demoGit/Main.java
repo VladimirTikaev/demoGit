cc22c518c6483ba08f44ce3752bfecd63a1eef6f
Vladimir Tikaev <vladimirt.215@gmail.com>
2022-07-03 10:37:31 +0300

    New text

--- src/main/java/com/overone/demoGit/Main.java
+++ src/main/java/com/overone/demoGit/Main.java
@@ -6,6 +6,7 @@ public class Main {
         System.out.println("Сегодня наше последнее занятие! Самое время выучить гит!");
         System.out.println("Квыпвомит для васи");
         System.out.println("new messaывпвываппge");
+        System.out.println("fkjgksjdgs;d");
 
     }
 }
