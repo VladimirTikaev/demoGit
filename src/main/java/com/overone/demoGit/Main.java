85143ff296b82f58fe22a91f43888c580884dd6c
Vladimir Tikaev <vladimirt.215@gmail.com>
2022-07-03 10:30:58 +0300

    Add new message

--- src/main/java/com/overone/demoGit/Main.java
+++ src/main/java/com/overone/demoGit/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Сегодня наше последнее занятие! Самое время выучить гит!");
         System.out.println("Комит для васи");
+        System.out.println("new message");
     }
 }
