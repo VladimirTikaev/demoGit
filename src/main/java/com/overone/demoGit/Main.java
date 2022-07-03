449409fe4cd533a75029dc6cf90e888adc8544f8
Vladimir Tikaev <vladimirt.215@gmail.com>
2022-07-03 10:13:25 +0300

    First commit

---
+++ src/main/java/com/overone/demoGit/Main.java
@@ -0,0 +1,8 @@
+package com.overone.demoGit;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Сегодня наше последнее занятие! Самое время выучить гит!");
+    }
+}
