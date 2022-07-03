ddc41bc87aad1c765f7de29d94bbe08305678d21
Vladimir Tikaev <vladimirt.215@gmail.com>
2022-07-03 10:17:20 +0300

    Second Commit

--- src/main/java/com/overone/demoGit/Main.java
+++ src/main/java/com/overone/demoGit/Main.java
@@ -4,5 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Сегодня наше последнее занятие! Самое время выучить гит!");
+        System.out.println("Комит для васи");
     }
 }
